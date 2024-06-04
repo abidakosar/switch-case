48a1bff26b3220e66be0056510f0076629c77922
abidakosar <161754486+abidakosar@users.noreply.github.com>
2024-06-04 05:46:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+"use strict";
+// for(let i=0;i<=5;i++){
+// console.log("naeem")
+// for(let j=1;j<=3;j++){
+// console.log("codhub")
+// }
+// }
+// let arr = ["haram","abrish","nida","abida"]
+// for (let i=0;i<=3;i++){
+// console.log("hy")
+// }
+// let arr =  ["ahmad","nida","haram","abrish","habib"]
+// let array = ["mango","banana","apple","orange"]
+// for(let i=0; i<=3; i++){
+// console.log(arr[i])
+// for(let j=0; j<=3; j++){
+// console.log(array[j])
+// }
+// }
+// let arr = ["APPLE","MANGO","BANANA","ORANGE"]
+// let array = ["abrish","habib","anum","maham"]
+// for(let i=0; i<=2; i++){
+// console.log(arr[i])
+// for(let j=0; j<=1; j++ ){
+// console.log(array[j])
+// }
+// }
+// function myname(para:string){
+// console.log(para)
+// }
+// myname("naeem")
+// function table(rows:number,col:number){
+// for(let i=0; i<= rows; i++){
+// for(let j=0; j<=col; j++){
+// let result= i*j
+// console.log(`${i} x ${j} = ${result}`)
+// }
+// console.log("\n")
+// }
+// }
+// table(10,10)
